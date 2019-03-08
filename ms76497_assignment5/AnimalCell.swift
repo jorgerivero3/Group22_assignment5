df37b98283e161c9654c8d8b73e3227e5596a43d
Jorge Rivero <riverojorge145@gmail.com>
2019-03-08 12:32:16 -0600

    Started working on collection view

---
+++ ms76497_assignment5/AnimalCell.swift
@@ -0,0 +1,13 @@
+//
+//  AnimalCell.swift
+//  ms76497_assignment5
+//
+//  Created by Jorge Rivero III on 3/8/19.
+//  Copyright © 2019 Slijepcevic, Milica. All rights reserved.
+//
+
+import UIKit
+
+class AnimalCell: UICollectionViewCell {
+    
+}
